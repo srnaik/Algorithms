f7a5951a1adcd66ef644719893009b3b4bfc8427
srnaik <srnaik@hotmail.com>
2018-09-14 21:52:05 -0700

    Implementing Binary Search Tree
    Implementing Binary Search Tree Recursively

---
+++ 23BinarySearchTree/src/com/sac/Node.java
@@ -0,0 +1,14 @@
+package com.sac;
+
+public class Node {
+
+    int key;
+    Node rightNode, leftNode;
+
+    public Node(int key){
+        this.key = key;
+        rightNode = null;
+        leftNode = null;
+    }
+
+}
