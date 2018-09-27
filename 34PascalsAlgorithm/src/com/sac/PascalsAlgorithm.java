35da9742ba10560d4f9e11fc2abd4ff8a0cc0099
srnaik <srnaik@hotmail.com>
2018-09-26 23:32:43 -0700

    Pascals Algorithm

---
+++ 34PascalsAlgorithm/src/com/sac/PascalsAlgorithm.java
@@ -0,0 +1,35 @@
+package com.sac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsAlgorithm {
+
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        if(numRows <= 0)
+            return result;
+
+        List<Integer> pre = new ArrayList<>();
+        pre.add(1);
+        result.add(pre);
+
+        for(int i = 2; i <=numRows;i++){
+            List<Integer> curr = new ArrayList<>();
+            curr.add(1);
+            for(int j = 0; j < pre.size()-1; j++){
+                curr.add(pre.get(j) + pre.get(j+1));
+            }
+            curr.add(1);
+            result.add(curr);
+            pre = curr;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        PascalsAlgorithm pascalsAlgorithm = new PascalsAlgorithm();
+        System.out.println(pascalsAlgorithm.generate(6));
+    }
+}
