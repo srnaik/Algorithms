e272249a84c593ca67768818701e250c47bc4b62
srnaik <srnaik@hotmail.com>
2019-06-08 15:05:06 -0700

    Implementing MaxHeap DataStructure

---
+++ 52NextGreaterElement01/src/com/sac/GreaterElement.java
@@ -0,0 +1,43 @@
+package com.sac;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class GreaterElement {
+
+    private Stack<Integer> stack = new Stack<>();
+    private Map<Integer,Integer> hsMap = new HashMap<>();
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2){
+
+        for(int num: nums2){
+            while (!stack.isEmpty() && stack.peek() < num){
+                hsMap.put(stack.pop(),num);
+            }
+            stack.push(num);
+        }
+
+        int[] result = new int[nums1.length];
+
+        for(int i = 0; i < nums1.length; i++){
+            result[i] = hsMap.getOrDefault(nums1[i],-1);
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        GreaterElement greaterElement = new GreaterElement();
+       int[]  nums1 = {2,4}, nums2 = {1,2,3,4};
+        int[] result = greaterElement.nextGreaterElement(nums1,nums2);
+
+        for (int num: result) {
+            System.out.println("Final Result is: " + num);
+        }
+
+
+    }
+
+}
