c5dcd879953df0931e73dffd63e83427719ae80b
srnaik <srnaik@hotmail.com>
2019-09-15 12:08:48 -0700

    Maximum and Minimum depth of a Binary Tree

--- 58ValidBinarySearchTree/src/com/sac/Solution.java
+++ 58ValidBinarySearchTree/src/com/sac/Solution.java
@@ -17,6 +17,28 @@ public class Solution {
                 && isValidBST(node.right, node.val, high);
     }
 
+    private int maxDepth(Node rootNode){
+
+        if(rootNode == null)
+            return 0;
+        if(rootNode.right == null)
+            return maxDepth(rootNode.left)+1;
+        if(rootNode.left == null)
+            return maxDepth(rootNode.right)+1;
+        return Integer.max(maxDepth(rootNode.left),maxDepth(rootNode.right))+1;
+    }
+
+    private int minDepth(Node rootNode){
+
+        if(rootNode == null)
+            return 0;
+        if(rootNode.right == null)
+            return maxDepth(rootNode.left)+1;
+        if(rootNode.left == null)
+            return maxDepth(rootNode.right)+1;
+        return Integer.min(maxDepth(rootNode.left),maxDepth(rootNode.right))+1;
+    }
+
 
     public static void main(String[] args) {
 
@@ -30,12 +52,21 @@ public class Solution {
         rootNode.right = new Node(24);
         rootNode.right.left = new Node(22);
         rootNode.right.right = new Node(26);
+        rootNode.right.right.right = new Node(28);
+        rootNode.right.right.left = new Node(25);
 
         boolean isBST = solution.isValidBST(rootNode);
         if(isBST)
             System.out.println("Given tree is a valid BST");
         else
             System.out.println("Given tree is not a valid BST");
+
+        int maxDepth = solution.maxDepth(rootNode);
+        System.out.println("Maximum Depth: "+ maxDepth);
+
+        int minDepth = solution.minDepth(rootNode);
+        System.out.println("Minimum Depth: "+ minDepth);
+
 
     }
 }
