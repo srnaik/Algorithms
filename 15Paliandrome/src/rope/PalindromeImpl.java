f1150ded8e1b5505303ad44650521ad23a583efe
srnaik <srnaik@hotmail.com>
2020-02-01 11:41:32 -0800

    Palindrome for Integers

--- 15Paliandrome/src/rope/PalindromeImpl.java
+++ 15Paliandrome/src/rope/PalindromeImpl.java
@@ -1,31 +1,72 @@
-package com.sac;
+package rope;
 
 import java.util.Scanner;
 
 public class PalindromeImpl {
 
 
-    public boolean isPalindrome(int number){
+    public boolean reverse(int number){
 
-        if(number < 0)
+        if(number < 0) {
             return false;
+        }
+
         int original = number, reverse = 0;
-        while(original != 0){
+
+        while(original > 0){
             reverse = reverse *10 + original%10;
             original /= 10;
         }
         return (number == reverse) ? true : false;
     }
 
+    public boolean isPalindrome(int number){
+
+        if(number< 0){
+            return false;
+        }
+
+        int div = 1;
+
+        while(number/div>=10){
+            div*= 10;
+        }
+
+        while(number != 0){
+
+            int left = number/div;
+            int right = number%10;
+
+            if(left!= right){
+                return false;
+            }
+
+            number = (number%div)/10;
+            div/=100;
+        }
+
+        return true;
+    }
+
+
+
+
     public static void main(String[] args) {
+
         PalindromeImpl palindrome = new PalindromeImpl();
         System.out.println("Enter an Integer number");
+
         Scanner scanner = new Scanner(System.in);
         int number = Integer.parseInt(scanner.nextLine());
         boolean isPalindrome = palindrome.isPalindrome(number);
-        if(isPalindrome)
+
+        if(isPalindrome) {
             System.out.println("The given number is palindrome");
-        else
+        }
+        else {
             System.out.println("The given number is not a palindrome");
+        }
+
     }
+
 }
