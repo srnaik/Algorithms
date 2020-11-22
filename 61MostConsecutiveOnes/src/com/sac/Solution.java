bdf8e270dcc090f55b011ec8b2e29ccf2b4c6d09
srnaik <srnaik@hotmail.com>
2020-11-21 19:18:56 -0800

    Most Consecutive Ones

---
+++ 61MostConsecutiveOnes/src/com/sac/Solution.java
@@ -0,0 +1,27 @@
+package com.sac;
+
+public class Solution {
+
+    public int findMaxConsecutiveOnes(int[] nums){
+
+        int count = 0, result = 0;
+
+        for (int num: nums) {
+            if(num == 1){
+                count++;
+                result = Math.max(count,result);
+            }else{
+                count=0;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] items = {1,0,1,1,1,0,0,1};
+        Solution solution = new Solution();
+        int result = solution.findMaxConsecutiveOnes(items);
+        System.out.println("Total Count: " + result);
+    }
+}
