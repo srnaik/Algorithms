106584d5b5e9a27b4ae46628ce988067928832dc
srnaik <srnaik@hotmail.com>
2022-01-20 21:05:36 -0800

    Code Indentation.

--- 97UniquePaths/src/com/sac/Solution.java
+++ 97UniquePaths/src/com/sac/Solution.java
@@ -4,24 +4,24 @@ import java.util.Arrays;
 
 public class Solution {
 
-    public int uniquePaths(int m, int n){
+    public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
 
-        for(int[] arr: dp){
-            Arrays.fill(arr,1);
+        for (int[] arr : dp) {
+            Arrays.fill(arr, 1);
         }
 
-        for(int col = 1; col < m; col++){
-            for(int row = 1; row < n; row++){
-                dp[col][row] = dp[col-1][row] + dp[col][row-1];
+        for (int col = 1; col < m; col++) {
+            for (int row = 1; row < n; row++) {
+                dp[col][row] = dp[col - 1][row] + dp[col][row - 1];
             }
         }
-        return dp[m-1][n-1];
+        return dp[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int result = solution.uniquePaths(3,2);
+        int result = solution.uniquePaths(3, 2);
         System.out.println("Maximum number of unique paths: " + result);
     }
 }
