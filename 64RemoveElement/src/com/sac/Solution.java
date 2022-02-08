0a430d4642e8d40c54b1395e57d1c431a0a7ab92
srnaik <srnaik@hotmail.com>
2022-02-07 21:38:14 -0800

    Remove duplicates

--- 64RemoveElement/src/com/sac/Solution.java
+++ 64RemoveElement/src/com/sac/Solution.java
@@ -7,12 +7,20 @@ public class Solution {
         int length = nums.length, index = 0;
 
         for (int i = 0; i < length; i++) {
-            if ( nums[i] != val) {
+            if (nums[i] != val) {
                 nums[index++] = nums[i];
             }
         }
-
         return index;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+
+        Solution solution = new Solution();
+        int val = solution.removeElement(nums, 2);
+        System.out.println("Array length after remvoing duplicates " + val);
 
     }
 }
